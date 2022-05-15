95f421f38a5467565fba94225caecb49e1e88ac6
nurhabibrs <nurhabibrizkisaputro@gmail.com>
2022-05-15 12:46:40 +0700

    Add form field

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,74 +8,57 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const appTitle = 'Register';
+
     return MaterialApp(
-      title: 'Register',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const MyHomePage(),
       ),
-      home: const MyHomePage(title: 'Register'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Form(
-        child: ListView(
-          padding: EdgeInsets.all(10.0),
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'Username'),
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'UserName'),
-            ),
-            RaisedButton(
-              onPressed: () {},
-              child: Text("Submit"),
-            )
-          ],
-        ),
+    return Form(
+      key: _formKey,
+      child: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Nama'),
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Alamat'),
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'No HP'),
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Email'),
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Password'),
+          ),
+          RaisedButton(
+            onPressed: () {},
+            child: Text("Register"),
+          )
+        ],
       ),
     );
   }
