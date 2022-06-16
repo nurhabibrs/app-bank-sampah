7a0c280b8bbda4ac222ef294a3cfc83de6c53e6b
nurhabibrs <nurhabibrizkisaputro@gmail.com>
2022-06-17 00:35:51 +0700

    Success use API to post data

--- lib/layers/register/fields/body.dart
+++ lib/layers/register/fields/body.dart
@@ -1,3 +1,5 @@
+import 'package:app_banksampah/layers/register/fields/dismisskeyboard.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import '../../../fields/rounded_button.dart';
@@ -14,15 +16,15 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  var _username;
-  var _fullname;
-  var _address;
-  var _phone;
-  var _email;
-  var _password;
+  // var _username;
+  // var _fullname;
+  // var _address;
+  // var _phone;
+  // var _email;
+  // var _password;
   bool _obscureText = true;
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _controllerUsername = TextEditingController();
   final TextEditingController _controllerFullname = TextEditingController();
   final TextEditingController _controllerAddress = TextEditingController();
@@ -36,9 +38,6 @@ class _BodyState extends State<Body> {
       controller: _controllerUsername,
       validator: (value) => checkUsername(value),
       maxLength: 255,
-      onSaved: (value) {
-        _username = value;
-      },
       decoration: const InputDecoration(
           hintText: "Username",
           labelText: "Username",
@@ -55,9 +54,6 @@ class _BodyState extends State<Body> {
       controller: _controllerFullname,
       validator: (value) => checkFullname(value),
       maxLength: 255,
-      onSaved: (value) {
-        _fullname = value;
-      },
       decoration: const InputDecoration(
           hintText: "Nama Lengkap",
           labelText: "Nama Lengkap",
@@ -74,9 +70,6 @@ class _BodyState extends State<Body> {
       controller: _controllerAddress,
       validator: (value) => checkAddress(value),
       maxLength: 255,
-      onSaved: (value) {
-        _address = value;
-      },
       decoration: const InputDecoration(
           hintText: "Alamat",
           labelText: "Alamat",
@@ -94,9 +87,6 @@ class _BodyState extends State<Body> {
       keyboardType: TextInputType.phone,
       validator: (value) => checkPhoneNumber(value),
       maxLength: 12,
-      onSaved: (value) {
-        _phone = value;
-      },
       decoration: const InputDecoration(
           hintText: "Nomor Handphone",
           labelText: "Nomor Handphone",
@@ -113,9 +103,6 @@ class _BodyState extends State<Body> {
       controller: _controllerEmail,
       keyboardType: TextInputType.emailAddress,
       validator: (value) => checkEmail(value),
-      onSaved: (value) {
-        _email = value;
-      },
       decoration: const InputDecoration(
           hintText: "Email",
           labelText: "Email",
@@ -131,9 +118,6 @@ class _BodyState extends State<Body> {
       controller: _controllerPassword,
       obscureText: _obscureText,
       validator: (value) => checkPassword(value),
-      onSaved: (value) {
-        _password = value;
-      },
       decoration: InputDecoration(
           hintText: "Password",
           labelText: "Password",
@@ -155,63 +139,55 @@ class _BodyState extends State<Body> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: ListView(
-                padding: const EdgeInsets.all(15.0),
-                children: <Widget>[
-                  buildUsernameField(),
-                  SizedBox(height: size.height * 0.02),
-                  buildFullnameField(),
-                  SizedBox(height: size.height * 0.02),
-                  buildAddressField(),
-                  SizedBox(height: size.height * 0.02),
-                  buildPhoneField(),
-                  SizedBox(height: size.height * 0.02),
-                  buildEmailField(),
-                  SizedBox(height: size.height * 0.02),
-                  buildPasswordField(),
-                  SizedBox(height: size.height * 0.02),
-                  RoundedButton(
-                    text: "REGISTER",
-                    color: const Color.fromARGB(255, 76, 202, 120),
-                    textColor: Colors.white,
-                    press: () {
-                      if (!_formKey.currentState!.validate()) {
-                        return;
-                      }
-                      _formKey.currentState!.save();
-
-                      // print(_username);
-                      // print(_fullname);
-                      // print(_address);
-                      // print(_phone);
-                      // print(_email);
-                      // print(_password);
-                    },
-                  ),
-                  RoundedButton(
-                    text: "CANCEL",
-                    color: const Color.fromARGB(255, 167, 211, 182),
-                    textColor: Colors.black,
-                    press: () {
-                      Navigator.push(
-                        context,
-                        PageTransition(
-                          type: PageTransitionType.leftToRight,
-                          child: const HomePage(),
-                        ),
-                      );
-                    },
-                  ),
-                ],
+      child: DismissKeyboard(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: ListView(
+                  padding: const EdgeInsets.all(15.0),
+                  children: <Widget>[
+                    buildUsernameField(),
+                    SizedBox(height: size.height * 0.02),
+                    buildFullnameField(),
+                    SizedBox(height: size.height * 0.02),
+                    buildAddressField(),
+                    SizedBox(height: size.height * 0.02),
+                    buildPhoneField(),
+                    SizedBox(height: size.height * 0.02),
+                    buildEmailField(),
+                    SizedBox(height: size.height * 0.02),
+                    buildPasswordField(),
+                    SizedBox(height: size.height * 0.02),
+                    RoundedButton(
+                      text: "REGISTER",
+                      color: const Color.fromARGB(255, 76, 202, 120),
+                      textColor: Colors.white,
+                      press: () {
+                        postUser();
+                      },
+                    ),
+                    RoundedButton(
+                      text: "CANCEL",
+                      color: const Color.fromARGB(255, 167, 211, 182),
+                      textColor: Colors.black,
+                      press: () {
+                        Navigator.push(
+                          context,
+                          PageTransition(
+                            type: PageTransitionType.leftToRight,
+                            child: const HomePage(),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -274,7 +250,7 @@ class _BodyState extends State<Body> {
   }
 
   checkPhoneNumber(String? value) {
-    // I just copied the pattern from here: https://regexr.com/3c53v
+    // Copied from: https://regexr.com/3c53v
     String pattern = r'^[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$';
     final regex = RegExp(pattern);
 
@@ -288,5 +264,35 @@ class _BodyState extends State<Body> {
       return 'Terlalu pendek.';
     }
     return null;
+  }
+
+  postUser() async {
+    var formData = FormData.fromMap(
+      {
+        'username': _controllerUsername.text,
+        'fullname': _controllerFullname.text,
+        'address': _controllerAddress.text,
+        'phone': _controllerPhone.text,
+        'email': _controllerEmail.text,
+        'password': _controllerPassword.text
+      },
+    );
+
+    Response response = await Dio().post(
+        'https://345d-103-23-224-196.ap.ngrok.io/register',
+        data: formData);
+
+    if (response.statusCode == 200) {
+      Navigator.push(
+        context,
+        PageTransition(
+          type: PageTransitionType.leftToRight,
+          child: const HomePage(),
+        ),
+      );
+    }
+
+    // check respons
+    print(response.data.toString());
   }
 }
