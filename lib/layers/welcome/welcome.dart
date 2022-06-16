c4f2b5334e2e91c9c047d24ef76241e0e9f73212
nurhabibrs <nurhabibrizkisaputro@gmail.com>
2022-06-16 16:43:29 +0700

    Edit welcome page

--- lib/layers/welcome/welcome.dart
+++ lib/layers/welcome/welcome.dart
@@ -6,6 +6,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Body());
+    return const Scaffold(
+      body: Body(),
+    );
   }
 }
